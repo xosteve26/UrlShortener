61fd062120a25a8110fe463f2ab68f64054419f0
Stephen George <stephengeorge2640@gmail.com>
2022-05-12 19:41:22 +0530

    Handling env

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db= config.get('mongo_URI');
+const dotenv = require('dotenv')
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true});
+        await mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true});
         console.log("MongoDB Connected...");
     }
     catch (err) {
