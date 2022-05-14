e260c143aabce8a12bae9bf7ce055106d32343f4
Stephen George <stephengeorge2640@gmail.com>
2022-05-14 20:17:01 +0530

    Introducing caching system with redis

--- routes/url.js
+++ routes/url.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const validUrl=require('valid-url');
 const shortid=require('shortid');
 const config=require('config');
+const redis = require('redis');
+
 
 const Url=require('../models/Url');
 
 //@route POST /api/url/shrink
 //@desc Shrink a URL
-router.post('/shrink',async (req,res)=>{
+router.post('/shrink',async (req,res)=>{   
     const { longUrl, expiration } = req.body
     const baseUrl = config.get('baseUrl');
     console.log(Date.now());
@@ -23,32 +25,43 @@ router.post('/shrink',async (req,res)=>{
 
     //Check long url
     if(validUrl.isUri(longUrl)){
-        try{
-            let url = await Url.findOne({ longUrl: longUrl });
-            if (url) {
-                res.json(url);
+        if(req.session[longUrl]){
+            console.log("IN IF")
+            res.json(req.session[longUrl])
+        }
+        else{
+            try {
+                let url = await Url.findOne({ longUrl: longUrl });
+                if (url) {
+                    res.json(url);
+                }
+                else {
+                    const shortUrl = baseUrl + '/' + urlCode;
+                    let currentTime = Date.now()
+                    const urlDocument = new Url({
+                        longUrl,
+                        shortUrl,
+                        urlCode,
+                        createdAt: new Date(currentTime),
+                        expireAt: expiration && new Date(currentTime + (expiration * 1000))
+                    });
+
+                    await urlDocument.save()
+                    req.session[longUrl] = urlDocument;
+                    console.log(req.session)
+
+                    res.status(200).json(urlDocument);
+                }
+
             }
-            else {
-                const shortUrl = baseUrl + '/' + urlCode;
-                let currentTime= Date.now()
-                const urlDocument = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    createdAt: new Date(currentTime),
-                    expireAt: expiration && new Date(currentTime+(expiration*1000)) 
-                });
-
-                await urlDocument.save()
-
-                res.status(200).json(urlDocument);
+            catch (err) {
+                console.error(err.message);
+                return res.status(500).json('Server Error');
             }
 
         }
-        catch(err){
-            console.error(err.message);
-            return res.status(500).json('Server Error');
-        }
+            
+        
          
     }else{
         res.status(401).json('Invalid Long Url');
